7d8a98836db826451aff4cf0a7abd5c74c876c9a
noload <vaibhavkamble023@gmail.com>
2023-12-30 13:27:00 +0530

    added middleware bodyparser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig')
 const setupAndStartServer = async ()=>{
 
     // create express object
     const app = express();
-    // const PORT = 3000;
+
+    // middleware
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+   
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
     })
