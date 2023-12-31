e88158e7995bb83f3b17934199cf2107c5209eac
noload <vaibhavkamble023@gmail.com>
2024-01-01 00:46:04 +0530

    completed city repository

--- src/respository/city-repository.js
+++ src/respository/city-repository.js
@@ -7,6 +7,7 @@ class CityRepository{
             const city = await City.create({name});
             return city;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error}
         }
     }
@@ -20,6 +21,29 @@ class CityRepository{
             });
         } catch (error) {
             throw{error}
+        }
+    }
+
+    async updateCity(cityId,data){
+        try {
+            const city = await City.update(data,{
+                where:{
+                    id:cityId
+                }
+            });
+        } catch (error) {
+            console.log("something went wrong");
+            throw {error}
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = await City.finByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("something went wrong");
+            throw {error}
         }
     }
 }
